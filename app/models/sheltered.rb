6442414abfecab6460f2e150c95b31289855db21
Leonardo G Siqueira <leonardo.prg@gmail.com>
2024-05-13 21:30:58 -0300

    adding gravatai to sheltered city

--- app/models/sheltered.rb
+++ app/models/sheltered.rb
@@ -9,5 +9,5 @@ class Sheltered < ApplicationRecord
 
   enum house_status: { total_loss: 'total_loss', partial_loss: 'partial_loss', flooded: 'flooded', other: 'other' }
   enum gender: { male: 'male', female: 'female', uninformed: 'uninformed' }
-  enum city: { cachoeirinha: 'cachoeirinha', porto_alegre: 'porto alegre', canoas: 'canoas' }
+  enum city: { cachoeirinha: 'cachoeirinha', porto_alegre: 'porto alegre', canoas: 'canoas', gravatai: 'gravatai' }
 end
