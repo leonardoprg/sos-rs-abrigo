2409ba541eb6b2c31a67952811d91ca2fac1e58f
Leonardo G Siqueira <leonardo.prg@gmail.com>
2024-05-14 21:34:37 -0300

    fix sheltered id

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -7,7 +7,7 @@ module HomeHelper
       sheltered_id: nil
     ).pluck(:sheltered_id).uniq.size.to_i
     alone_people = current_user.organization.sheltereds.active.where.not(
-      id: Sheltered.select(:sheltered_id)
+      id: current_user.organization.sheltereds.active.pluck(:sheltered_id)
     ).where(sheltered_id: nil).count.to_i
 
     families + alone_people
