c7a33792f1368e3c3c87b138a196c138f585ffbb
João Pereira <jomialvespereira@gmail.com>
2024-05-15 20:05:09 +0100

    Update test to use factory

--- test/models/medicin_test.rb
+++ test/models/medicin_test.rb
@@ -4,18 +4,19 @@ require 'test_helper'
 
 class MedicinTest < ActiveSupport::TestCase
   test 'should be valid with a name' do
-    medicin = Medicin.new(name: 'Paracetamol')
+    medicin = FactoryBot.create(:medicin)
     assert medicin.valid?
   end
 
   test 'should be invalid without a name' do
-    medicin = Medicin.new(name: nil)
+    medicin = FactoryBot.create(:medicin)
+    medicin.name = nil
     assert_not medicin.valid?
     assert_equal ['não pode ficar em branco'], medicin.errors[:name]
   end
 
   test 'should be invalid with a duplicate name' do
-    Medicin.create(name: 'Paracetamol')
+    FactoryBot.create(:medicin, name: 'Paracetamol')
     medicin = Medicin.new(name: 'Paracetamol')
     assert_not medicin.valid?
     assert_equal ['já está em uso'], medicin.errors[:name]
