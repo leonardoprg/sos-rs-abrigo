52024a11d294a195bf6308061b0e88f6c212c0a7
Leonardo G Siqueira <leonardo.prg@gmail.com>
2024-05-14 21:45:46 -0300

    fix report to only show active sheltereds

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,6 +4,6 @@ class ReportsController < ApplicationController
   layout 'report'
 
   def sheltereds
-    @sheltereds = current_user.organization.sheltereds.order(:name)
+    @sheltereds = current_user.organization.sheltereds.active.order(:name)
   end
 end
